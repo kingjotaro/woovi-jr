fca315185a4f401c12ff9ffb244d3f2b561a69b6
rafael <rafaelleet@gmail.com>
2024-07-11 07:04:59 -0300

    add second header cmponente

---
+++ src/app/components/headerDynamic.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function HeaderDynamic() {
+  return (
+    <div className=' text-[24px] font-extrabold text-[#4D4D4D]'>
+      Contratar Rafael por um ano, como deseja pagar?
+    </div>
+  );
+}
+
+export default HeaderDynamic;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Footer from "./components/footer";
 import Header from "./components/header";
+import HeaderDynamic from "./components/headerDynamic";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen flex-col items-center justify-between p-24">
       <Header></Header>
+      <HeaderDynamic></HeaderDynamic>
     hello
 
     <Footer></Footer>
