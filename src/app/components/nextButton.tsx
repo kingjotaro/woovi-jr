54264c1c71f5781978f03658a6cb0e07c94617c1
rafael <rafaelleet@gmail.com>
2024-07-16 04:49:42 -0300

    add next button

---
+++ src/app/components/nextButton.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { usePathname, useRouter } from 'next/navigation';
+
+
+function FixedNavigationButton({ isVisible}:  any ) {
+  const router = useRouter();
+
+  const pathname = usePathname()
+
+  function next() {
+    if (pathname === "/") {
+      router.push('/pixpayment');
+    }
+    if (pathname === '/pixpayment') {
+      router.push('/creditpayment');
+    }
+  
+    
+  }
+
+  return (
+    isVisible && (
+      <button
+        className="border-customGreen left-4 p-2 w-24 h-10 bg-customGreen text-white rounded-full shadow-lg font-bold shadow-lg shadow-gren-500/50"
+        onClick={next}
+      >
+        Próximo
+      </button>
+    )
+  );
+}
+
+export default FixedNavigationButton;
