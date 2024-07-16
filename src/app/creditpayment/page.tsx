0f3cb454cd7a16019eda9061bd78c2b583f28217
rafael <rafaelleet@gmail.com>
2024-07-16 04:54:16 -0300

    renaming

---
+++ src/app/creditpayment/page.tsx
@@ -0,0 +1,33 @@
+'use client'
+import Footer from "../components/footer";
+import Header from "../components/header";
+import HeaderDynamic from "../components/headerDynamic";
+import InstallmentComponent from "../components/Installment";
+import PaymentForm from "../components/paymentForm";
+import PreviousButton from "../components/previousButton";
+import PaymentDeadline from "../components/timeLimit";
+import TotalComponent from "../components/custoTotal";
+
+
+export default function Page2() {
+
+ const headerPage = `João, pague o restante em 1x no cartão`
+
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-between  min-w-screen">
+      <Header/>
+      <HeaderDynamic header={headerPage}></HeaderDynamic>
+      <PaymentForm/>
+     
+      <PaymentDeadline/> 
+      <InstallmentComponent/>
+      <TotalComponent/>
+      <PreviousButton/>
+      
+   
+    <Footer></Footer>
+    </div>
+    
+  
+  );
+}
---
+++ src/app/pixpayment/page.tsx
@@ -0,0 +1,40 @@
+'use client'
+import Footer from "../components/footer";
+import Header from "../components/header";
+import HeaderDynamic from "../components/headerDynamic";
+import InstallmentComponent from "../components/Installment";
+import FixedNavigationButton from "../components/nextButton";
+import PreviousButton from "../components/previousButton";
+import Qrcode from "../components/qrcode";
+import PaymentDeadline from "../components/timeLimit";
+import TotalComponent from "../components/custoTotal";
+
+
+export default function Page2() {
+
+ const headerPage = `João, pague a entrada de
+R$ 15.300,00 pelo Pix`
+
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-between  min-w-screen">
+      <Header></Header>
+      <HeaderDynamic header={headerPage}></HeaderDynamic>
+      <Qrcode></Qrcode>
+      
+      <PaymentDeadline></PaymentDeadline> 
+      <InstallmentComponent></InstallmentComponent>
+      <TotalComponent></TotalComponent>
+    
+      <div className="flex flex-row items-center gap-2">
+      <PreviousButton/>
+      <FixedNavigationButton isVisible={true}></FixedNavigationButton>
+      </div>
+      
+      
+   
+    <Footer></Footer>
+    </div>
+    
+  
+  );
+}
