769ff52d964fbfac35ff4632f8f23ddc4fd8f973
rafael <rafaelleet@gmail.com>
2024-07-16 04:50:19 -0300

    add qrcode generator and reader component

--- src/app/components/qrcode.tsx
+++ src/app/components/qrcode.tsx
@@ -1,16 +1,68 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import QRCode from 'qrcode.react';
+import jsQR from 'jsqr';
+import Image from 'next/image';
 
+const Qrcode = () => {
 
-function qrcode() {
-    const qrValue = "https://example.com";
+  const mock = {
+    name: "rafael",
+    email: "rafael@.com",
+    phone: "3333-3333",
+  };
+  const qrRef = useRef<HTMLDivElement>(null);
 
-    return (
-      <div>
-        <h1>Gerador de QR Code</h1>
-        <QRCode value={qrValue} />
-      </div>
-    );
+  const handleDecode = () => {
+    if (qrRef.current) {
+      const canvas = qrRef.current.querySelector('canvas');
+      if (canvas) {
+        const context = canvas.getContext('2d');
+        if (context) {
+          const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+          const code = jsQR(imageData.data, imageData.width, imageData.height);
+          if (code) {
+
+            copyToClipboard(code.data);
+          }
+        }
+      }
+    }
   };
 
-export default qrcode
+  const copyToClipboard = (text: string) => {
+    navigator.clipboard.writeText(text).then(() => {
+      alert('QRCODE copiado para a área de transferência!');
+    }).catch(err => {
+      console.error('Falha ao copiar: ', err);
+    });
+  };
+
+  const paymentData = JSON.stringify(mock);
+
+  return (
+    <div>
+
+      <div>
+        <div ref={qrRef}>
+          <QRCode value={paymentData} size={332}  />
+        </div>
+      </div>
+
+      <div 
+    className='flex justify-center border bg-blue-900 text-white  rounded-md  mt-2 border-black'>
+        <button onClick={handleDecode}>Copiar QR Code</button>
+        <div className=''>
+        <Image
+        priority
+        src="/copy.svg"
+        height={20}
+        width={20}
+        alt="copy"
+      />
+      </div>
+      </div>
+    </div>
+  );
+};
+
+export default Qrcode;
